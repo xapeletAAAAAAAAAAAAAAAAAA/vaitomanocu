ab391804bbe23b22f1fdcbe8f0f2b7b844a1d369
natyyp <natypiresoliveira@gmail.com>
2023-05-12 15:05:15 -0300

    first commit

---
+++ vaitomanocu.js
@@ -0,0 +1,51 @@
+// const idade = Number(prompt("insira sua idade!"))
+// if(idade>=18){
+//     console.log("voce pode tirar carteira de motorista!")
+// }
+// else{
+//     console.log("voce não pode tirar carteira de motorista");
+// }
+
+
+// const turno = prompt("para sabermos o turno onde estuda digite M,V,N! ")
+// if(turno === "M"){
+//     console.log("Bom Dia!")
+// }
+// if(turno === "V"){
+//     console.log("Boa Tarde!")
+// }
+// if(turno === "N"){
+//     console.log("Boa Noite!")
+// }
+
+
+// const turno = prompt("para sabermos o turno onde estuda digite M(matutino),V(vespertino) ou N(noturno)! ")
+// switch(turno.toUpperCase()){
+//     case "V":
+//     console.log("boa tarde!");
+//     break;
+//     case "M":
+//     console.log("bom dia!");
+//     break;
+//     case "N":
+//     console.log("boa noite!");
+//     break;
+//     default:
+//     console.log("tem que digitar M,V ou N seu cabaço");            
+//     break;
+//     }
+
+
+
+// const putaquepariu = prompt("Qual gênero de filme deseja assistir?").toLowerCase()
+// const vaitomanocu = Number(prompt("insira o preço do ingresso"))
+//     if(putaquepariu === "fantasia" && vaitomanocu <= 15){
+//         console.log("Tenha Um Òtimo Filme!");
+//     }  
+//     if(vaitomanocu > 15){
+
+//     console.log("Escolha Algo Mais Barato!");
+//     }
+//     if(putaquepariu !== "fantasia"){
+//     console.log("Escolha Outro Filme!");
+//     }
